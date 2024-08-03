57200ce66402f348f01d2a7c450b315f703dd38d
Nensi-Goswami <nen5588@gmail.com>
2024-08-03 22:42:36 +0530

    Day 2 Notes

---
+++ day_2.py
@@ -0,0 +1,7 @@
+student_name = "Nensi"
+hindi_marks = 44
+maths_marks = 35
+science_marks = 44
+maths_2_marks = 35
+total_marks = hindi_marks + maths_marks + science_marks + maths_2_marks
+print(f"Student Name-{student_name} Total Makrks-{total_marks}")
